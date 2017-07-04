cd6818e9985a1fe8fbc95877e3cda681155e33f5
liyang <1289714862@qq.com>
2017-07-04 19:00:01 +0800

    init

--- initsql/test.sql
+++ initsql/test.sql
@@ -1,5 +1,11 @@
-CREATE TABLE `t_label` (
-  `id` bigint(20) NOT NULL COMMENT 'id',
-  `labelCode` bigint(20) NOT NULL COMMENT 'code'
+SET FOREIGN_KEY_CHECKS=0;
 
+-- ----------------------------
+-- Table structure for t_label1
+-- ----------------------------
+DROP TABLE IF EXISTS `t_label1`;
+CREATE TABLE `t_label1` (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `labelCode` varchar(20) NOT NULL COMMENT 'code',
+  `org_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
